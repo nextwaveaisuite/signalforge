9c0c4aeea7973541dbdeeb45971979e1168288d7
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-12-09 14:58:24 +1000

    page.tsx

--- app/build-board/page.tsx
+++ app/build-board/page.tsx
@@ -1,10 +1,21 @@
-async function getData() {
-  const res = await fetch(process.env.NEXT_PUBLIC_URL + '/api/build-board', { cache: 'no-store' });
-  return res.json();
-}
-export default async function BuildBoard() {
-  const data = await getData();
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export default function BuildBoard() {
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch('/api/build-board')
+      .then(res => res.json())
+      .then(setData)
+      .catch(() => setData([]));
+  }, []);
+
   return (
-    <pre>{JSON.stringify(data, null, 2)}</pre>
+    <div>
+      <h2>Build Board</h2>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
   );
 }
