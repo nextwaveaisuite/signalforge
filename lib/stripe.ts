24803d9d9b57f3decb1c925ae1a1bb7e52920d9f
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-12-10 13:11:11 +1000

    stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,9 +1,8 @@
 import Stripe from "stripe";
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error("STRIPE_SECRET_KEY is not set");
-}
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2023-10-16",
-});
+export const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY as string,
+  {
+    apiVersion: "2023-10-16",
+  }
+);
