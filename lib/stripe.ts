83c44286583a6114d86503b50e9fedf81b351540
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-12-11 14:30:40 +1000

    stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,5 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2023-10-16",
 });
-
-export default stripe;
